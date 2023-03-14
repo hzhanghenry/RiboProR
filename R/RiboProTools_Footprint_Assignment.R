464e04a707df5242e75cc47b41e125798b5f3241
hzhanghenry <hzhang@mail.nih.gov>
2023-03-14 13:45:44 -0400

    Update RiboProTools_Footprint_Assignment.R

--- R/RiboProTools_Footprint_Assignment.R
+++ R/RiboProTools_Footprint_Assignment.R
@@ -1,55 +1,54 @@
 #*
-#*		File: RiboProfiling_Footprint_Assignment.R
+#*    File: RiboProfiling_Footprint_Assignment.R
 #*
-#*		Calculate frame size for transcripts defined in annotation file
-#*		(bed format) and for reads in bam file aligned to the transcripts
+#*    Calculate frame size for transcripts defined in annotation file
+#*    (bed format) and for reads in bam file aligned to the transcripts
 #*
-#*		Source code from supplyment of reference:
+#*    Source code from supplyment of reference:
 #*
-#*		Nicholas T. Ingolia, Gloria A. Brar, Noam Stern-Ginossar, Michael S.  
-#*		Harris, Gaelle J.S. Talhouarne, Sarah E. Jackson, Mark R. Wills,
-#*		and Jonathan S. Weissman (2014). Ribosome Profiling Reveals Pervasive
-#*		Translation Outside of Annotated Protein-Coding Genes. Cell Reports  
-#*		8, 1365~1379.
+#*    Nicholas T. Ingolia, Gloria A. Brar, Noam Stern-Ginossar, Michael S.  
+#*    Harris, Gaelle J.S. Talhouarne, Sarah E. Jackson, Mark R. Wills,
+#*    and Jonathan S. Weissman (2014). Ribosome Profiling Reveals Pervasive
+#*    Translation Outside of Annotated Protein-Coding Genes. Cell Reports  
+#*    8, 1365~1379.
 #*
-#*		Function defined:
+#*    Function defined:
 #*
-#*		1.	alignASites()
-#*		2.	getAlignASites()
-#*		3.	getASiteProfile()
-#*		4.	trxRegionCountSizes()
-#*		5.	trxRegionCountAligns()
-#*		6.	countAligns()
-#*		7.	countSizes()
-#*		8.	regionCountFrame()
+#*    1.  alignASites()
+#*    2.  getAlignASites()
+#*    3.  getASiteProfile()
+#*    4.  trxRegionCountSizes()
+#*    5.  trxRegionCountAligns()
+#*    6.  countAligns()
+#*    7.  countSizes()
+#*    8.  regionCountFrame()
 #*
-#*		Last revised on April 18, 2018
-#*	___________________________________________________________________________
-#*	<fp assignment><fp assignment><fp assignment><fp assignment><fp assignment>
+#*    Last revised on April 18, 2018
+#*  ___________________________________________________________________________
+#*  <fp assignment><fp assignment><fp assignment><fp assignment><fp assignment>
 
 
 
 
 
     
-#*	=========================================================================
+#*  =========================================================================
 #*
-#*		1.	alignASites()
+#*    1.  alignASites()
 #*
-#*		Covert GAligments objects for a transcript to a GAlignments object
-#*		having the A site nucleotides only.
+#*    Covert GAligments objects for a transcript to a GAlignments object
+#*    having the A site nucleotides only.
 #*
-#*		Arguments:
+#*    Arguments:
 #*
-#*			asiteOffsets:	A data frame of one column with row names are 
-#*							fragment length and column is for A sites.
-#*			alns:			A GAlignments objects for a transcript.
+#*        asiteOffsets:  A data frame of one column with row names are 
+#*                       fragment length and column is for A sites.
+#*        alns:          A GAlignments objects for a transcript.
 #*
-#*		Return:		A GAlignments object for A site nucleotides only.
+#*    Return:  A GAlignments object for A site nucleotides only.
 #*
 #*
-alignASites <- function(asiteOffsets, alns) 
-{
+alignASites <- function(asiteOffsets, alns)  {
     alnLengths <- qwidth(alns)
     alnAOffs <- asiteOffsets[as.character(alnLengths), "asite"]
 
@@ -85,84 +84,79 @@ alignASites <- function(asiteOffsets, alns)
 #*			A GAlignments object for A site nucleotides only.
 #*
 #*
-getAlignASites <- function(asiteOffsets, bamfile, trx) 
-{
-	alignASites(asiteOffsets, getAlignments(bamfile, trx))
+getAlignASites <- function(asiteOffsets, bamfile, trx)  {
+    alignASites(asiteOffsets, getAlignments(bamfile, trx))
 }
 
 
-#*	=========================================================================
+#*  =========================================================================
 #*
-#*		3.	getASiteProfile()
+#*    3.  getASiteProfile()
 #*
-#*		Covert aligned A sites to a table for relative position of A sites
-#*		in each transcript
+#*    Covert aligned A sites to a table for relative position of A sites
+#*    in each transcript
 #*
-#*		Arguments:
+#*    Arguments:
 #*
-#*			asiteOffsets:	A data frame of one column with row names are 
-#*							fragment length and column is for A sites.
-#*			bamfle:			character vector, bam file name and path.
-#*			trx:			GRange object for a transcript.
+#*        asiteOffsets:	 A data frame of one column with row names are 
+#*                       fragment length and column is for A sites.
+#*        bamfle:        character vector, bam file name and path.
+#*        trx:           GRange object for a transcript.
 #*
-#*		Return:		
+#*    Return:		
 #*
-#*			Positive integer vector, a site status for each nucleotides
-#*			of the transcript.
+#*        Positive integer vector, a site status for each nucleotides
+#*        of the transcript.
 #*
 #*
-getASiteProfile <- function(asiteOffsets, bamfile, trx) 
-{
-	asites <- getAlignASites(asiteOffsets, bamfile, trx)
+getASiteProfile <- function(asiteOffsets, bamfile, trx)  {
+    asites <- getAlignASites(asiteOffsets, bamfile, trx)
 
-	if (length(asites) > 0) { 
-		tabulate( relativeWithin(start(asites), trx),
-			nbins = sum(width(trx)) )
-	} else {
-		tabulate( as.integer(c()), nbins = sum(width(trx)) )
-  }
+    if (length(asites) > 0) { 
+        tabulate( relativeWithin(start(asites), trx),
+            nbins = sum(width(trx)) )
+    } else {
+        tabulate( as.integer(c()), nbins = sum(width(trx)) )
+    }
 }
 
 
-#*	=========================================================================
+#*  =========================================================================
 #*
-#*		4.	trxRegionCountSizes()
+#*    4.  trxRegionCountSizes()
 #*
-#*		Calculate size of whole transcript, cds, 5'UTR, and 3'UTR of a 
-#*		transcript in bed file.
+#*    Calculate size of whole transcript, cds, 5'UTR, and 3'UTR of a 
+#*    transcript in bed file.
 #*
-#*		Arguments:
+#*    Arguments:
 #*
-#*			insets:	List of integers, insets in nucleotides to avoid  
-#*					start and stop positions. 
-#*			trx:	GRange object for transcripts, output from import()
-#*			cds:	IRanges object for cds.
+#*        insets:  List of integers, insets in nucleotides to avoid  
+#*                 start and stop positions. 
+#*        trx:     GRange object for transcripts, output from import()
+#*        cds:     IRanges object for cds.
 #*
-#*		Return:
-#*
-#*			List of integers for sizes of cds, 5-UTR, and 3-UTR.
+#*    Return:   List of integers for sizes of cds, 5-UTR, and 3-UTR.
 #*
 #*
-trxRegionCountSizes <- function(insets, trx, cds) 
-{
-	sizes <- list( trx = sum(width(trx)) )
+trxRegionCountSizes <- function(insets, trx, cds)  {
+
+    sizes <- list( trx = sum(width(trx)) )
     
-	if (length(cds) == 1) 
-	{
-		cdsQStart <- start(cds) + insets$cdsInset5
-		cdsQEnd   <- end(cds) - insets$cdsInset3
+    if (length(cds) == 1)  {
+        cdsQStart <- start(cds) + insets$cdsInset5
+        cdsQEnd   <- end(cds) - insets$cdsInset3
         
-		sizes$cds <- if (cdsQEnd < cdsQStart) { 
-				NA 
-		} else { 1 + cdsQEnd - cdsQStart }
+        sizes$cds <- if (cdsQEnd < cdsQStart) { 
+            NA 
+        } else { 1 + cdsQEnd - cdsQStart }
         
-		utr5QEnd   <- start(cds) - (insets$utr5Inset3 + 1)
-		sizes$utr5 <- if (utr5QEnd < 0) { NA } else { 1 + utr5QEnd }
+        utr5QEnd   <- start(cds) - (insets$utr5Inset3 + 1)
+        sizes$utr5 <- if (utr5QEnd < 0) { NA } else { 1 + utr5QEnd }
 
-		utr3QStart <- end(cds) + (insets$utr3Inset5 + 1)
-		sizes$utr3 <- if (utr3QStart >= sum(width(trx))) { 
-				NA 
-		} else { 1 + sum(width(trx)) - utr3QStart }
+        utr3QStart <- end(cds) + (insets$utr3Inset5 + 1)
+        sizes$utr3 <- if (utr3QStart >= sum(width(trx))) { 
+            NA 
+        } else { 1 + sum(width(trx)) - utr3QStart }
     } else {
         sizes$cds  <- NA
         sizes$utr5 <- NA
@@ -173,77 +167,74 @@ trxRegionCountSizes <- function(insets, trx, cds)
 }
 
 
-#*	=========================================================================
+#* =========================================================================
 #*
-#*		5.	trxRegionCountAligns()
+#*    5.	trxRegionCountAligns()
 #*
-#*		Check out if reads of a transcript has A site. If yes, get the total 
-#*		counts for transcript, cds, 5-UTR, and 3-UTR. Otherwise, set counts 
-#*		of the transcript to 0, and 0 or NA( if no cds defined) for cds, 5-UTR, 
-#*		and 3-UTR.
+#*    Check out if reads of a transcript has A site. If yes, get the total 
+#*    counts for transcript, cds, 5-UTR, and 3-UTR. Otherwise, set counts 
+#*    of the transcript to 0, and 0 or NA( if no cds defined) for cds, 5-UTR, 
+#*    and 3-UTR.
 #*
-#*		Arguments:
+#*    Arguments:
 #*		
-#*		asiteOffsets:	A data frame of one column with row names are 
-#*						fragment length and column is for A sites.
-#*		insets:			List of integer, insets in nucleotides for    
-#*						avoiding start and stop. There are two build-in  
-#*						insets: default and zero insets.
-#*		bamfile: 		Character vector, bam file name and path.
-#*		trx:			GRange object for a transcript.
-#*		cds:			IRange object for cds in the transcript.
+#*        asiteOffsets:	 A data frame of one column with row names are 
+#*                       fragment length and column is for A sites.
+#*        insets:        List of integer, insets in nucleotides for    
+#*                       avoiding start and stop. There are two build-in  
+#*                       insets: default and zero insets.
+#*        bamfile:       Character vector, bam file name and path.
+#*        trx:           GRange object for a transcript.
+#*        cds:           IRange object for cds in the transcript.
 #*
-#*		Return:
-#*
-#*			List of sizes for cds, 5-UTR, and 3-UTR of a transcript
+#*    Return:  List of sizes for cds, 5-UTR, and 3-UTR of a transcript
 #*
 #*
-trxRegionCountAligns <- function(asiteOffsets, insets, bamfile, trx, cds) 
-{
+trxRegionCountAligns <- function(asiteOffsets, insets, bamfile, trx, cds) {
+
     genomicAsites <- getAlignASites(asiteOffsets, bamfile, trx);
 
-    if (length(genomicAsites) > 0) 
-	{
-		asites <- relativeWithin(start(genomicAsites), trx);
-		counts <- list( trx = sum(!is.na(asites)) );
+    if (length(genomicAsites) > 0) {
+
+        asites <- relativeWithin(start(genomicAsites), trx);
+        counts <- list( trx = sum(!is.na(asites)) );
         
-		if ( length(cds) == 1 ) 
-		{
-			cdsQStart <- start(cds) + insets$cdsInset5;
-			cdsQEnd   <- end(cds) - insets$cdsInset3;
+        if ( length(cds) == 1 ) {
+            cdsQStart <- start(cds) + insets$cdsInset5;
+            cdsQEnd   <- end(cds) - insets$cdsInset3;
             
-			counts$cds <- if (cdsQEnd < cdsQStart) { 
+            counts$cds <- if (cdsQEnd < cdsQStart) { 
 				NA 
-			} else { 
-				sum(asites >= cdsQStart & asites <= cdsQEnd, na.rm=TRUE) 
-			}
+            } else { 
+                sum(asites >= cdsQStart & asites <= cdsQEnd, na.rm=TRUE) 
+            }
             
-			utr5QEnd <- start(cds) - (insets$utr5Inset3 + 1);
-			counts$utr5 <- if (utr5QEnd < 0) { 
-				NA 
-			} else { 
-				sum(asites <= utr5QEnd, na.rm=TRUE) 
-			}
+            utr5QEnd <- start(cds) - (insets$utr5Inset3 + 1);
+            counts$utr5 <- if (utr5QEnd < 0) { 
+                NA 
+            } else { 
+                sum(asites <= utr5QEnd, na.rm=TRUE) 
+            }
             
-			utr3QStart  <- end(cds) + (insets$utr3Inset5 + 1);
-			counts$utr3 <- if (utr3QStart >= sum(width(trx))) { 
-				NA 
-			} else { 
-				sum(asites >= utr3QStart, na.rm=TRUE) 
-			}
+            utr3QStart  <- end(cds) + (insets$utr3Inset5 + 1);
+            counts$utr3 <- if (utr3QStart >= sum(width(trx))) { 
+                NA 
+            } else { 
+                sum(asites >= utr3QStart, na.rm=TRUE) 
+            }
         } else {
             counts$cds  <- NA
             counts$utr5 <- NA
             counts$utr3 <- NA           
         }
     } else {
-		counts <- list( trx = 0 )
+        counts <- list( trx = 0 )
 
-		if ( length(cds) == 1) {
+        if ( length(cds) == 1) {
             counts$cds <- 0
             counts$utr5 <- 0
             counts$utr3 <- 0
-		} else {
+        } else {
             counts$cds <- NA
             counts$utr5 <- NA
             counts$utr3 <- NA
@@ -254,42 +245,41 @@ trxRegionCountAligns <- function(asiteOffsets, insets, bamfile, trx, cds)
 }
 
 
-#*	=========================================================================
+#*  =========================================================================
 #*
-#*		6.	countAligns()
+#*    6.  countAligns()
 #*
-#*		Count the reads in a bam file which hits transcript, cds, 5-UTR,
-#*		and 3-UTR for all transcripts. 
+#*    Count the reads in a bam file which hits transcript, cds, 5-UTR,
+#*    and 3-UTR for all transcripts. 
 #*
-#*		Arguments:
+#*    Arguments:
 #*		
-#*			asiteOffsets:	A data frame of one column with row names are 
-#*							fragment length and column is for A sites.
-#*			insets:			List of integer, insets in nucleotides for    
-#*							avoiding start and stop position. There are   
-#*							two build-in insets: default and zero insets.
-#*			bamfile: 		Character vector, bam file name and path.
-#*			trxBed:			GRange object for all transcripts.
+#*        asiteOffsets:  A data frame of one column with row names are 
+#*                       fragment length and column is for A sites.
+#*        insets:        List of integer, insets in nucleotides for    
+#*                       avoiding start and stop position. There are   
+#*                       two build-in insets: default and zero insets.
+#*        bamfile:       Character vector, bam file name and path.
+#*        trxBed:        GRange object for all transcripts.
 #*
-#*		Return:	
+#*    Return:	
 #*
-#*			List of positive integers, counts for whole transcript, cds,  
-#*			5-UTR, and 3-UTR of all transcripts.  
+#*        List of positive integers, counts for whole transcript, cds,  
+#*        5-UTR, and 3-UTR of all transcripts.  
 #*
 #*
-countAligns <- function(asiteOffsets, insets, bamfile, trxBed) 
-{
-	ttl <- length(trxBed)
-	countOne <- function(i) 
-	{
-		if (i %% 100 == 0) { 
-			print(c(i, ttl, as.character(trxBed$name[i]))) 
-		}
-			trxGR <- transcriptGRanges(trxBed[i])
-			cdsIR <- transcriptCdsIRanges(trxGR, 
-							start(trxBed$thick)[[i]], 
-							end(trxBed$thick)[[i]]);
-			trxRegionCountAligns(asiteOffsets, insets, bamfile, trxGR, cdsIR);
+countAligns <- function(asiteOffsets, insets, bamfile, trxBed) {
+
+    ttl <- length(trxBed)
+    countOne <- function(i) {
+        if (i %% 100 == 0) { 
+            print(c(i, ttl, as.character(trxBed$name[i]))) 
+        }
+        trxGR <- transcriptGRanges(trxBed[i])
+        cdsIR <- transcriptCdsIRanges(trxGR, 
+            start(trxBed$thick)[[i]], 
+            end(trxBed$thick)[[i]]);
+        trxRegionCountAligns(asiteOffsets, insets, bamfile, trxGR, cdsIR);
     }
     counts <- mclapply(seq(1,ttl), countOne)
     names(counts) <- trxBed$name
@@ -297,30 +287,29 @@ countAligns <- function(asiteOffsets, insets, bamfile, trxBed)
 }
 
 
-#*	=========================================================================
+#*  =========================================================================
 #*
-#*		7.	countSizes()
+#*    7.  countSizes()
 #*
-#*		Count transcript sizes for each transcript in bed file. Output will
-#*		be used for GetRegionCountFrame()
+#*    Count transcript sizes for each transcript in bed file. Output will
+#*    be used for GetRegionCountFrame()
 #*		
-#*		Arguments:
+#*    Arguments:
 #*
-#*			insets:	list of integer, insets in nucleotides for avoiding   
-#*					star tand stop.    
-#*			trxBed:	GRange object of all transcripts.
+#*        insets:  list of integer, insets in nucleotides for avoiding   
+#*                 star tand stop.    
+#*        trxBed:  GRange object of all transcripts.
 #*
-#*		Return: 
+#*    Return: 
 #*
-#*			List of sizes of cds, 5-UTR, and 3-UTR of all transcript.
+#*        List of sizes of cds, 5-UTR, and 3-UTR of all transcript.
 #*
 #*
-countSizes <- function(insets, trxBed) 
-{
+countSizes <- function(insets, trxBed) {
     countOne <- function(i) {
         trxGR <- transcriptGRanges(trxBed[i])
         cdsIR <- transcriptCdsIRanges(trxGR, start(trxBed$thick)[[i]], 
-						end(trxBed$thick)[[i]]);
+                    end(trxBed$thick)[[i]]);
         trxRegionCountSizes(insets, trxGR, cdsIR);
     }    
     sizes <- mclapply(seq(1,length(trxBed)), countOne);
@@ -329,24 +318,25 @@ countSizes <- function(insets, trxBed)
 }
 
 
-#*	=========================================================================
+#*  =========================================================================
 #*
-#*		8.	regionCountFrame()
+#*    8.  regionCountFrame()
 #*
-#*		Covert lists for sizes of transcript, cds, 5-UTR, and 3-UTR to
-#*		a data frame.
+#*    Covert lists for sizes of transcript, cds, 5-UTR, and 3-UTR to
+#*    a data frame.
 #*
-#*		Argument:	
-#*		List of integer lists, read counts in cds, 5-UTR, and 3-UTR of   
-#*		each transcript.
+#*    Argument:	
 #*
-#*		Return: 
-#*		Data frame with rows for transcript names and columns for size of
-#*		transcript, cds, 5-UTR, and 3-UTR.
+#*        List of integer lists, read counts in cds, 5-UTR, and 3-UTR of   
+#*        each transcript.
+#*
+#*    Return: 
+#*
+#*        A data frame with rows for transcript names and columns for size
+#*        of transcript, cds, 5-UTR, and 3-UTR.
 #*
 #*
-regionCountFrame <- function(counts) 
-{
+regionCountFrame <- function(counts)  {
     data.frame(trx  = unlist(lapply(counts, function(r) { r$trx })),
                cds  = unlist(lapply(counts, function(r) { r$cds })),
                utr5 = unlist(lapply(counts, function(r) { r$utr5 })),
@@ -356,6 +346,6 @@ regionCountFrame <- function(counts)
 
 
 #*
-#*	End of RiboProfiling_Footprints_Assignment.R
-#*	___________________________________________________________________________
-#*	<fp assignment><fp assignment><fp assignment><fp assignment><fp assignment>
+#*  End of RiboProfiling_Footprints_Assignment.R
+#*  ___________________________________________________________________________
+#*  <fp assignment><fp assignment><fp assignment><fp assignment><fp assignment>
